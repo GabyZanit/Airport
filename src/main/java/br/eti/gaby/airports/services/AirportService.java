30783447f9914926040a5b127ebed17e7418322e
Gaby <gabriellyzanit.senai@gmail.com>
2025-05-21 17:03:32 -0300

    Término da Fase 3 – Implementado endpoint FindByCityIgnoreCase. - Update AirportRepository – Adicionado assinatura de método findByCityIgnoreCase; - Update AirportService – Adicionado método findByCity - Update AirportController – Adicionado endpoint /city/{cityName}

--- src/main/java/br/eti/gaby/airports/services/AirportService.java
+++ src/main/java/br/eti/gaby/airports/services/AirportService.java
@@ -10,17 +10,21 @@ import org.springframework.stereotype.Service;
  *
  * @author ppjatb
  */
-
 @Service
 public class AirportService {
-    
+
     @Autowired
     private AirportRepository airportRepository;
-    
+
     public List<Airport> findAll() {
-        
+
         List<Airport> result = airportRepository.findAll();
         return result;
+
     }
 
+    public List<Airport> findByCity(String city) {
+        List<Airport> result = airportRepository.findByCityIgnoreCase(city);
+        return result;
+    }
 }
